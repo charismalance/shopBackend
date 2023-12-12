f1991521ac81ad3293d281ac2dcb65191ab2ca15
alireza <alirezaazarkaman13671998@gmail.com>
2023-12-12 20:13:17 +0330

    sql push

---
+++ shop.sql
@@ -0,0 +1,326 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 12, 2023 at 05:42 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Basekets`
+--
+
+CREATE TABLE `Basekets` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) DEFAULT NULL,
+  `uesrId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Catgories`
+--
+
+CREATE TABLE `Catgories` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Catgories`
+--
+
+INSERT INTO `Catgories` (`id`, `title`, `createdAt`, `updatedAt`) VALUES
+(1, 'تلفن همراه', '2023-12-08 08:14:21', '2023-12-08 08:14:21'),
+(2, 'لوازم خانگی', '2023-12-08 08:14:21', '2023-12-08 08:14:21'),
+(3, 'صنایع دستی', '2023-12-08 08:14:21', '2023-12-08 08:14:21'),
+(4, 'کامپیوتر', '2023-12-08 08:14:21', '2023-12-08 08:14:21'),
+(5, 'لوازم جانبی', '2023-12-08 08:14:21', '2023-12-08 08:14:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Currencies`
+--
+
+CREATE TABLE `Currencies` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Currencies`
+--
+
+INSERT INTO `Currencies` (`id`, `title`, `createdAt`, `updatedAt`) VALUES
+(1, 'تومان', '2023-12-08 11:45:20', '2023-12-08 11:45:20'),
+(2, 'ریال', '2023-12-08 11:45:20', '2023-12-08 11:45:20'),
+(3, 'درصد', '2023-12-08 11:45:20', '2023-12-08 11:45:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Orders`
+--
+
+CREATE TABLE `Orders` (
+  `id` int(11) NOT NULL,
+  `selerId` int(11) DEFAULT NULL,
+  `currenyId` int(11) DEFAULT NULL,
+  `catgoiesId` int(11) DEFAULT NULL,
+  `count` varchar(255) DEFAULT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`body`)),
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Orders`
+--
+
+INSERT INTO `Orders` (`id`, `selerId`, `currenyId`, `catgoiesId`, `count`, `body`, `createdAt`, `updatedAt`) VALUES
+(1, 2, 3, 1, '100', '{\r\n\"price\" : \"5000\"\r\n}', '2023-12-12 19:42:27', '2023-12-12 19:42:27'),
+(2, 2, 2, 5, '20', NULL, '2023-12-12 19:57:14', '2023-12-12 19:57:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Roles`
+--
+
+CREATE TABLE `Roles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SequelizeMeta`
+--
+
+CREATE TABLE `SequelizeMeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `SequelizeMeta`
+--
+
+INSERT INTO `SequelizeMeta` (`name`) VALUES
+('20210919092900-create-user.js'),
+('20210919093130-create-role.js'),
+('20210919093157-create-user-role.js'),
+('20231208075103-create-catgories.js'),
+('20231208075204-create-currency.js'),
+('20231208082308-create-order.js'),
+('20231208083702-create-baseket.js');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `UserRoles`
+--
+
+CREATE TABLE `UserRoles` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `nationalCode` varchar(255) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `verificationCode` varchar(255) DEFAULT NULL,
+  `verificationCodeExpireTime` datetime DEFAULT NULL,
+  `isVerified` varchar(255) DEFAULT NULL,
+  `townshipId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `username`, `email`, `password`, `phone`, `firstName`, `lastName`, `nationalCode`, `gender`, `verificationCode`, `verificationCodeExpireTime`, `isVerified`, `townshipId`, `createdAt`, `updatedAt`) VALUES
+(2, 'mayam@kordloo.com', 'mayam@kordloo.com', '$2b$10$eLQCO7nTy2nW1rMbbJPx7uzzB0bCeRmEQpsKaOAnHd84XpyVvUHOa', '09199559728', 'mayayam', 'kordlo', NULL, NULL, NULL, NULL, NULL, NULL, '2023-12-08 08:14:21', '2023-12-08 08:14:21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Basekets`
+--
+ALTER TABLE `Basekets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderId` (`orderId`),
+  ADD KEY `uesrId` (`uesrId`);
+
+--
+-- Indexes for table `Catgories`
+--
+ALTER TABLE `Catgories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Currencies`
+--
+ALTER TABLE `Currencies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Orders`
+--
+ALTER TABLE `Orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `selerId` (`selerId`),
+  ADD KEY `currenyId` (`currenyId`),
+  ADD KEY `catgoiesId` (`catgoiesId`);
+
+--
+-- Indexes for table `Roles`
+--
+ALTER TABLE `Roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `SequelizeMeta`
+--
+ALTER TABLE `SequelizeMeta`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `UserRoles`
+--
+ALTER TABLE `UserRoles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `roleId` (`roleId`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Basekets`
+--
+ALTER TABLE `Basekets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Catgories`
+--
+ALTER TABLE `Catgories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `Currencies`
+--
+ALTER TABLE `Currencies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Orders`
+--
+ALTER TABLE `Orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `Roles`
+--
+ALTER TABLE `Roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `UserRoles`
+--
+ALTER TABLE `UserRoles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Basekets`
+--
+ALTER TABLE `Basekets`
+  ADD CONSTRAINT `Basekets_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `Orders` (`id`),
+  ADD CONSTRAINT `Basekets_ibfk_2` FOREIGN KEY (`uesrId`) REFERENCES `Users` (`id`);
+
+--
+-- Constraints for table `Orders`
+--
+ALTER TABLE `Orders`
+  ADD CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`selerId`) REFERENCES `Users` (`id`),
+  ADD CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`currenyId`) REFERENCES `Currencies` (`id`),
+  ADD CONSTRAINT `Orders_ibfk_3` FOREIGN KEY (`catgoiesId`) REFERENCES `Catgories` (`id`);
+
+--
+-- Constraints for table `UserRoles`
+--
+ALTER TABLE `UserRoles`
+  ADD CONSTRAINT `UserRoles_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `UserRoles_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `Roles` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
